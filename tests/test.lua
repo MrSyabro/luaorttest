443fad773b7af1b980ab59da7178cd18cc061dd4
MrSyabro <syabro1998@gmail.com>
2024-05-26 20:44:14 +0300

    feat: обновление pngutils

--- tests/test.lua
+++ tests/test.lua
@@ -1,21 +1,25 @@
 local Ort = require "luaort" --[[@as Ort]]
 local png = require "luapng" --[[@as PNGUtils]]
 
-local w, h, d = png.read("in.png")
-print("Png size", w, h)
-print("string size", #d)
+local dw, dh, d = png.read("girl.png")
 d = png.hwc2chw(d)
+print("Data size", #d)
+
+local maskw, maskh, maskdata = png.read_grayscale("mask.png")
+maskdata = png.hwc2chw(maskdata)
+print("Mask2 size", #maskdata)
 
 local Env = Ort.CreateEnv()
 
 local SessionOptions = Ort.CreateSessionOptions()
 
-local Session = Env:CreateSession("candy.onnx", SessionOptions)
+print("Loading model")
+local Session = Env:CreateSession("lama_fp32.onnx", SessionOptions)
 
 local inputs = Session:GetInputs()
 print("Inputs:")
 for i, name in ipairs(inputs) do
-	local datatype, dims = Session:GetOutputType(1)
+	local datatype, dims = Session:GetInputType(i)
 	io.write(("%d %s(%s): {"):format(i, name, datatype))
 	for i, dim in ipairs(dims) do
 		io.write(tostring(dim))
@@ -27,7 +31,7 @@ end
 local outputs = Session:GetOutputs()
 print("Outputs:")
 for i, name in ipairs(outputs) do
-	local datatype, dims = Session:GetOutputType(1)
+	local datatype, dims = Session:GetOutputType(i)
 	io.write(("%d %s(%s): {"):format(i, name, datatype))
 	for i, dim in ipairs(dims) do
 		io.write(tostring(dim))
@@ -36,8 +40,13 @@ for i, name in ipairs(outputs) do
 	io.write("}\n")
 end
 
-local inputvalue = Ort.CreateValue({ 1, 3, w, h }, "FLOAT", d)
-local outputvalues = Session:Run(inputs, {inputvalue}, outputs)
+--print("PAUSE"); io.read("l")
+
+local inputvalue = Ort.CreateValue({ 1, 3, dw, dh }, "FLOAT", d)
+local maskvalue = Ort.CreateValue({1, 1, maskw, maskh}, "FLOAT", maskdata)
+
+print("Running")
+local outputvalues = Session:Run(inputs, {inputvalue, maskvalue}, outputs)
 
 local d = outputvalues[1]:GetData()
 print("size of outdata", #d)
@@ -45,6 +54,6 @@ print("size of outdata", #d)
 d = png.chw2hwc(d)
 print("data2 size", #d)
 
-png.write(d, w, h, "out.png")
+png.write(d, dw, dh, "out.png")
 
 os.execute("start out.png")
